3cf7613b6cfc45d1e2e9f987c25442738069eb3c
Regeq <regeq@seznam.cz>
2026-02-02 19:38:20 +0100

    Fixed not working \n argument

--- src/map_commands.cpp
+++ src/map_commands.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 void cmd_echo(const vector<string>& args)
 {
-    for (const auto& arg : args)
-        cout << arg << ' ';
-    cout << endl;
+    for (int i = 0; i < args.size(); i++)
+    {
+        if (args[i] == "\\n") {
+            cout << endl;
+        }
+        else {
+            cout << args[i] << ' ';
+        }
+    }
 }
 
