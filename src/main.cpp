8c2ae342879483c48b2f19a5181cc396d4f0ff00
regeq <regeq@seznam.cz>
2026-05-15 21:08:06 +0200

    patched

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <format>
 
 #include "../include/shell_dispatcher.h"
+#include "db/db_handling.h"
+
+#define CYAN "\033[36m"
+#define RED "\033[31m"
+#define PURPLE "\033[35m"
+#define RESET "\033[0m"
 
 using namespace std;
 
@@ -42,22 +49,25 @@ static vector<string> tokenize_input(const string& input_shell)
 int main()
 {
     Shell shell;
+    Database db;
     startDispatching(shell);
 
-    const string device = "esko@eskotop";
+    const string currentCharacter = "NULL";
     const string shell_symbol = " >>";
-    string input_shell, command, dir = "Desktop";
+    string input_shell, command;
+    int hp = NULL, lvl = NULL;
 
     while (true)
     {
-        cout << "\033[36m" << device
-             << "\033[31m" << " ~/" << dir
-             << "\033[35m" << shell_symbol
-             << " \033[0m";
+        cout << CYAN << currentCharacter
+             << RED << format(" [{} HP | LVL {}]", hp, lvl)
+             << PURPLE << shell_symbol
+             << RESET;
 
         getline(cin, input_shell);
 
         if (input_shell == "exit")
+            //  db.save();
             break;
 
         auto tokens = tokenize_input(input_shell);
